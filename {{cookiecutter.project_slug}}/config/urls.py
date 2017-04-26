2bb91dbea0bc420fd047008f75fbca0863338582
Michael A. Gonzalez <genomics.geek.04.22@gmail.com>
2017-04-26 21:23:03 +0000

    template is in base directory

--- {{cookiecutter.project_slug}}/config/urls.py
+++ {{cookiecutter.project_slug}}/config/urls.py
@@ -16,7 +16,7 @@ from {{ cookiecutter.project_slug }}.users.views import VerifyEmailView
 schema_view = get_swagger_view(title='{{ cookiecutter.project_name }} API')
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='pages/index.html'), name='index'),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
     url(r'^home/$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
 
